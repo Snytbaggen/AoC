af874e63a7ea470d07619ec6f84ae71ca327dfbc
Daniel Häggmyr <danha479@student.liu.se>
2022-12-09 11:28:08 +0100

    Day 1

---
+++ utils.py
@@ -0,0 +1,12 @@
+def splitFile(filename, regex):
+    with open(filename) as f:
+        return filter(None, f.read().split(regex))
+
+def splitString(source, regex):
+    return list(filter(None, source.split(regex)))
+
+def splitStringAsNumbers(source, regex):
+    return list(map(int, splitString(source, regex)))
+
+def sumStringList(list):
+    return sum(map(int, list))
