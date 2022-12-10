bbf1cf0ecbd6da18a438fab45918fffd60801aea
Daniel Häggmyr <snytbaggen2@hotmail.comr>
2022-12-10 09:08:04 +0100

    Day 10

---
+++ day_10_state.py
@@ -0,0 +1,12 @@
+instructionCycles = {
+    "addx": 2,
+    "noop": 1
+}
+
+cpuCycles = 0
+programCounter = -1
+instructionCounter = 0
+instructions = []
+currentInstruction = []
+registerX = 1
+halt = False
